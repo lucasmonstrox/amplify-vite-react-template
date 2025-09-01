691e08e213c7df57a6528a2ae998f522a763dcb9
João Galiano <joao.galiano.silva@gmail.com>
2025-09-01 05:29:22 +0100

    refactor: atualizo next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  experimental: {
-    serverComponentsExternalPackages: ["@aws-amplify/ui-react"],
-  },
-  // Configuração para o Amplify
-  trailingSlash: false,
-  // Desabilitar otimização de imagens se não estiver usando
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
